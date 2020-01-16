5c510d9cad5529b8d4cfc121c933be641e463d54
Nerevarishe <JIyHaTuK@gmail.com>
2020-01-16 22:37:09 +0300

    delete unnecesary files and migrating to hooks

--- src/NewsPage/AddNewsPage/AddNewsPage.js
+++ src/NewsPage/AddNewsPage/AddNewsPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router"
 
 import CKEditor from "@ckeditor/ckeditor5-react";
@@ -9,50 +9,45 @@ import editorConfiguration from "../../CKEditorConf";
 import Save from "../../components/buttons/Save";
 import {axiosInstance as axios} from "../../axiosInstance";
 
-class AddNewsPage extends Component {
-  state = {
-    newsPost: '',
-    redirectToNewsPage: false
-  };
+const AddNewsPage = (props) => {
 
-  saveDataHandler = () => {
-    axios.post('/news/', {"postBody": this.state.newsPost})
-      .then(response => {
-        console.log(response);
-        this.setState({redirectToNewsPage: true})})
-      .catch(error => console.log(error))
-  };
+  const [newsPost, setNewsPost] = useState('');
+  const [redirectToNewsPage, setRedirectToNewsPage] = useState(false);
 
-  render() {
-    if (this.state.redirectToNewsPage) {
-      return <Redirect to={'/news/'}/>
-    }
-    return (
-      <div>
-        <p>Add News Page</p>
-        <CKEditor
-          editor={ClassicEditor}
-          config={editorConfiguration}
-          data={this.props.data}
-          onInit={editor => {
-            editor.editing.view.focus()
-          }}
-          onChange={(event, editor) => {
-            const data = editor.getData();
-            console.log({ event, editor, data });
-            this.setState({newsPost: data})
-          }}
-          // onBlur={(event, editor) => {
-          //   console.log("Blur.", editor);
-          // }}
-          // onFocus={(event, editor) => {
-          //   console.log("Focus.", editor);
-          // }}
-        />
-        <Save btnClicked={this.saveDataHandler}/>
-      </div>
-    );
+  const saveDataHandler = () => {
+    setNewsPost(prevState =>
+      axios.post('/news/', {"postBody": newsPost})
+        .then(response => {
+          console.log(response);
+          setRedirectToNewsPage(true)
+        })
+        .catch(error => console.log(error))
+    )
+
+    };
+
+  if (redirectToNewsPage) {
+    return <Redirect to={'/news/'}/>
   }
-}
+  return (
+    <div>
+      <p>Add News Page</p>
+      <CKEditor
+        editor={ClassicEditor}
+        config={editorConfiguration}
+        data={props.data}
+        onInit={editor => {
+          editor.editing.view.focus()
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log({event, editor, data});
+          setNewsPost(data)
+        }}
+      />
+      <Save btnClicked={saveDataHandler}/>
+    </div>
+  );
+};
 
 export default AddNewsPage;
