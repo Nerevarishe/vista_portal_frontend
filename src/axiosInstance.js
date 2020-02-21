e1c5231d1c7269404dfd25db4be1ee42d8eec7ed
Nerevarishe <JIyHaTuK@gmail.com>
2020-02-21 11:46:36 +0300

    Automatic Refresh tokens after access token expire

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -15,56 +15,42 @@ const axiosInstance = axios.create(axiosBaseConfig);
 const axiosWithoutInterceptors = axios.create(axiosBaseConfig);
 
 // Interceptors for base axios instance
-axiosInstance.interceptors.request.use(
-  async config => {
-    // Send auth token in all requests by default
-    const accessToken = await loadAccessToken();
-    if (accessToken) {
-      config.headers['Authorization'] = `Bearer ${accessToken["accessToken"]}`;
-    }
-    return config
-  },
-  error => Promise.reject(error)
-);
-
-let attempt = 0;
+// axiosInstance.interceptors.request.use(
+//   async config => {
+//     // Send auth token in all requests by default
+//     const accessToken = await loadAccessToken();
+//     if (accessToken) {
+//       config.headers['Authorization'] = `Bearer ${accessToken["accessToken"]}`;
+//     }
+//     return Promise.resolve(config)
+//   },
+//   error => Promise.reject(error)
+// );
 
 axiosInstance.interceptors.response.use(
   response => response,
   async (error) => {
     // Get original request config
-    const originalRequest = error.config;
-    // TODO: Comment this part of code
-    if (error.response.status === 401 && attempt === 1) {
-      // router.push('/auth/login');
-      attempt = 0;
-      console.log("Try redirect");
-      return <Redirect to={'/login'} />
-      // return Promise.reject(error)
-    }
-
-    // If server response with 401 error code and originalRequest._retry is false:
+    let originalRequest = error.config;
     if (error.response.status === 401 && !originalRequest._retry) {
-      attempt++;
       originalRequest._retry = true;
       const refreshToken = await loadRefreshToken();
+      const data = {};
       const conf = {
         headers: {
           "Authorization": `Bearer ${refreshToken["refreshToken"]}`
         }
       };
-      return axiosWithoutInterceptors.post('/auth/refresh', {}, conf)
+      return axiosInstance.post("/auth/refresh", data, conf)
         .then(async response => {
-          if (response.status === 200) {
-            await saveAccessToken(response.data["access_token"]);
-            await saveRefreshToken(response.data["refresh_token"]);
-            const accessToken = response.data["access_token"];
-            axiosInstance.defaults.headers['Authorization'] = `Bearer ${accessToken["accessToken"]}`;
-            return axiosInstance(originalRequest)
-          }
+          await saveRefreshToken(response.data["refresh_token"]);
+          await saveAccessToken(response.data["access_token"]);
+          originalRequest.headers["Authorization"] = `Bearer ${response.data["access_token"]}`;
+          return axiosInstance(originalRequest)
         })
-        .catch(error => Promise.reject(error));
+        .catch(console.log)
     }
+    return Promise.reject(error)
   }
 );
 
