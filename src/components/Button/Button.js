97ad2bc5a985b9e809312f08db118da063395994
Nerevarishe <JIyHaTuK@gmail.com>
2020-02-22 20:23:01 +0300

    Create default button style

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,8 @@
 import React from "react";
+import classes from "./Button.module.css"
 
 const Button = props => (
-  <button id={props.id} onClick={props.clicked} disabled={props.btnDisabled}>
+  <button id={props.id} onClick={props.clicked} disabled={props.btnDisabled} className={classes["basicButton"]}>
     {props.text}
   </button>
 );
