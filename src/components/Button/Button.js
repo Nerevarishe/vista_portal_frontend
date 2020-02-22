169ebbe6ca020d22f68805e85cb854826c8fb320
Nerevarishe <JIyHaTuK@gmail.com>
2020-02-22 20:48:55 +0300

    button disabled style

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,16 @@
 import React from "react";
 import classes from "./Button.module.css"
 
-const Button = props => (
-  <button id={props.id} onClick={props.clicked} disabled={props.btnDisabled} className={classes["basicButton"]}>
-    {props.text}
-  </button>
-);
+const Button = props => {
+  let style = [classes["basicButton"]];
+  if (props.btnDisabled) {
+    style = [classes["disabled"]]
+  }
+  return (
+    <button id={props.id} onClick={props.clicked} disabled={props.btnDisabled} className={style}>
+      {props.text}
+    </button>
+  );
+};
 
 export default Button;
