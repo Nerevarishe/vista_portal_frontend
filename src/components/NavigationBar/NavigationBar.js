00ba15a084c11e4bf72368c0b995b324731c79d7
Nerevarishe <JIyHaTuK@gmail.com>
2020-03-20 16:08:54 +0300

    prototype navbar

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
 import { Context } from "../../stores/store";
 import { Link } from "react-router-dom";
-import classes from "./NavigationBar.module.css";
+import AppBar from "@material-ui/core/AppBar"
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-const NavigationBar = () => {
+const NavigationBar = (props) => {
   const [state, dispatch] = useContext(Context);
   const username = state.auth["username"];
   let navBarUser = <Link to="/login">Войти</Link>;
@@ -16,22 +18,29 @@ const NavigationBar = () => {
 
   return (
     <React.Fragment>
-      <nav>
-        <ul className={classes["nav-bar__nav-part"]}>
-          <li>
-            <Link to="/news">Новости</Link>
-          </li>
-          <li>
-            <Link to="/defectura">Дефектура</Link>
-          </li>
-          <li>
-            <a href="/wiki">Вики</a>
-          </li>
-        </ul>
-        <ul className={classes["nav-bar__user-part"]}>
-          {navBarUser}
-        </ul>
-      </nav>
+      {/*<nav>*/}
+      {/*  <ul className={classes["nav-bar__nav-part"]}>*/}
+      {/*    <li>*/}
+      {/*      <Link to="/news">Новости</Link>*/}
+      {/*    </li>*/}
+      {/*    <li>*/}
+      {/*      <Link to="/defectura">Дефектура</Link>*/}
+      {/*    </li>*/}
+      {/*    <li>*/}
+      {/*      <a href="/wiki">Вики</a>*/}
+      {/*    </li>*/}
+      {/*  </ul>*/}
+      {/*  <ul className={classes["nav-bar__user-part"]}>*/}
+      {/*    {navBarUser}*/}
+      {/*  </ul>*/}
+      {/*</nav>*/}
+      <AppBar position="fixed">
+        <Tabs value={props.location.pathname} >
+          <Tab label="Новости" />
+          <Tab label="Деффектура"/>
+          <Tab label="Вики"/>
+        </Tabs>
+      </AppBar>
     </React.Fragment>
   );
 };
--- src/components/RenderComponents/RenderComponents.js
+++ src/components/RenderComponents/RenderComponents.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react';
 import NavigationBar from "../NavigationBar";
 import TryLoginAfterIndexPageFirstLoad from "../../Auth/TryLoginAfterIndexPageFirstLoad";
-import {Route, Router, Switch} from "react-router-dom";
+import {Route, Router, Switch, Link} from "react-router-dom";
 import NewsPage from "../../containers/NewsPage";
 import LoginPage from "../../containers/LoginPage";
 import Login from "../../Auth/Login";
@@ -13,6 +13,10 @@ import {history} from "../../App";
 import {Context} from "../../stores/store";
 import Modal from "../Modal";
 
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 const RenderComponents = () => {
   const [state, dispatch] = useContext(Context);
   let modal = state.modal.showModal ? <Modal /> : null;
@@ -21,18 +25,34 @@ const RenderComponents = () => {
     <React.Fragment>
       { modal }
       <Router history={history}>
-        <NavigationBar />
+        {/*<NavigationBar />*/}
         <TryLoginAfterIndexPageFirstLoad />
-        <Switch>
-          <Route exact path="/" component={NewsPage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/login/auth" component={Login} />
-          <Route exact path="/logout" component={Logout} />
-          <Route exact path="/news" component={NewsPage} />
-          <Route exact path="/news/add_news_post" component={AddNewsPage} />
-          <Route exact path="/defectura" component={DefecturaPage} />
-          <Route path="*" component={NotFoundPage} />
-        </Switch>
+
+        <Route
+          path="/"
+          render={({ location }) => (
+            <React.Fragment>
+              <AppBar position="sticky">
+                <Tabs value={location.pathname}>
+                  <Tab label="Новости" value="/" component={Link} to={'/'} />
+                  <Tab label="Деффектура" value="/defectura" component={Link} to={'/defectura'} />
+                  <Tab label="Вики" value="/wiki" component={Link} to={'/wiki'} />
+                  <Tab label="Войти" value="/Login" component={Link} to={'/login'} />
+                </Tabs>
+              </AppBar>
+              <Switch>
+                <Route exact path="/" component={NewsPage} />
+                <Route exact path="/login" component={LoginPage} />
+                <Route exact path="/login/auth" component={Login} />
+                <Route exact path="/logout" component={Logout} />
+                <Route exact path="/news" component={NewsPage} />
+                <Route exact path="/news/add_news_post" component={AddNewsPage} />
+                <Route exact path="/defectura" component={DefecturaPage} />
+                <Route path="*" component={NotFoundPage} />
+              </Switch>
+            </React.Fragment>
+          )}
+        />
       </Router>
     </React.Fragment>
   );
