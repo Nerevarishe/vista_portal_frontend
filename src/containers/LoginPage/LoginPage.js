03ca2e38fba024c360b0a87f6431f54703c1d9d7
Nerevarishe <JIyHaTuK@gmail.com>
2020-04-26 14:21:45 +0300

    react-bootstrap to LoginPage

--- src/containers/LoginPage/LoginPage.js
+++ src/containers/LoginPage/LoginPage.js
@@ -1,7 +1,12 @@
 import React, { useState, useContext } from "react";
 import { history } from "../../App";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
-import Button from "../../components/Button";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
@@ -17,16 +22,42 @@ const LoginPage = () => {
 
   return (
     <React.Fragment>
-      <p>Login Page</p>
-      <input
-        type="text"
-        onChange={(event) => setUsername(event.target.value)}
-      />
-      <input
-        type="password"
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <Button text="Send" clicked={loginHandler} />
+      <Container>
+        <Row>
+          <Col xs="6" className="m-auto">
+            <Card>
+              <Card.Body>
+                <Form>
+                  <Form.Row>
+                    <Form.Group as={Col} xs="auto" className="m-auto">
+                      <Form.Label>Username:</Form.Label>
+                      <Form.Control
+                        placeholder="Enter username"
+                        onChange={(event) => setUsername(event.target.value)}
+                      />
+                    </Form.Group>
+                  </Form.Row>
+                  <Form.Row className="mt-2">
+                    <Form.Group as={Col} xs="auto" className="m-auto">
+                      <Form.Label>Password:</Form.Label>
+                      <Form.Control
+                        type="password"
+                        placeholder="Enter password"
+                        onChange={(event) => setPassword(event.target.value)}
+                      />
+                    </Form.Group>
+                  </Form.Row>
+                  <Form.Row className="mt-2">
+                    <Button onClick={loginHandler} className="m-auto">
+                      Log In
+                    </Button>
+                  </Form.Row>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 };
