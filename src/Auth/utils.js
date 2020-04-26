82b7d24a9a311978bfb586d63f0258ba45236bc3
Nerevarishe <JIyHaTuK@gmail.com>
2020-04-26 19:38:14 +0300

    fix autoLogin

--- src/Auth/utils.js
+++ src/Auth/utils.js
@@ -1,6 +1,5 @@
 import { axiosInstance as axios } from "../config/axiosInstance";
 import { db, REFRESH_TOKEN, ACCESS_TOKEN } from "../config/dexieConfig";
-import { history } from "../App";
 
 const saveRefreshToken = async (refreshToken) => {
   await db.tokensTable.put({ id: 1, refreshToken: refreshToken });
@@ -61,9 +60,9 @@ const autoLoginUser = async () => {
   if (response.status === 200) {
     await saveAccessToken(response.data["access_token"]);
     await saveRefreshToken(response.data["refresh_token"]);
-    const base64Url = response.data["access_token"].split(".")[1];
-    const decodedValue = JSON.parse(window.atob(base64Url));
-    return Promise.resolve(decodedValue["identity"]);
+    return Promise.resolve(
+      await userIdentityFromAccessToken(response.data["access_token"])
+    );
   }
   return Promise.reject();
 };
