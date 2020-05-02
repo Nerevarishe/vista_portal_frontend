a3296f3a4ec666cf05ffd029d2744d884d6a62f9
Nerevarishe <JIyHaTuK@gmail.com>
2020-05-02 12:28:50 +0300

    temp

--- src/containers/DefecturaPage/DefecturaPage.js
+++ src/containers/DefecturaPage/DefecturaPage.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
-import moment from "moment";
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../../stores/store";
 
+import moment from "moment";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 
+import Button from "react-bootstrap/Button";
 import {
   fetchDefectura,
   addDefectura,
@@ -14,6 +15,7 @@ import {
   toggleZD,
   delDefecturaDayCard,
 } from "./utils";
+import { deleteNewsPost } from "../NewsPage/utils";
 
 const DefecturaPage = () => {
   const [drugName, setDrugName] = useState("");
@@ -23,6 +25,7 @@ const DefecturaPage = () => {
   const [needFetchDefectura, setNeedFetchDefectura] = useState(0);
   const [defData, setDefData] = useState(null);
   const [drugsInZD, setDrugsInZD] = useState(null);
+  const [state, dispatch] = useContext(Context);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -48,13 +51,37 @@ const DefecturaPage = () => {
       setDisableButton(false);
     }
   };
-  const delDefecturaRecordHandler = async (event) => {
+  const delDefecturaRecordHandler = async (id) => {
     setDisableButton(true);
-    const isDefecturaRecordDeleted = await delDefectura(event.target.id);
-    if (isDefecturaRecordDeleted) {
-      setNeedFetchDefectura((prevState) => prevState + 1);
-      setDisableButton(false);
-    }
+    dispatch({
+      type: "DELETE_DEFECTURA_RECORD_MODAL",
+      data: [
+        // Array that contains:
+        // Element id that must be deleted
+        id,
+        // function, which exec when button 'Yes' pressed in modal
+        async () => {
+          const response = await delDefectura(id);
+          if (response.status === 200) {
+            dispatch({ type: "RESET_MODAL" });
+            setNeedFetchDefectura((prevState) => prevState + 1);
+            setDisableButton(false);
+          }
+          dispatch({ type: "RESET_MODAL" });
+          setDisableButton(false);
+        },
+        // function, which exec when button 'No' pressed in modal
+        () => {
+          dispatch({ type: "RESET_MODAL" });
+          setDisableButton(false);
+        },
+      ],
+    });
+    // const isDefecturaRecordDeleted = await delDefectura(event.target.id);
+    // if (isDefecturaRecordDeleted) {
+    //   setNeedFetchDefectura((prevState) => prevState + 1);
+    //   setDisableButton(false);
+    // }
   };
 
   const toggleZDHandler = async (event) => {
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -64,6 +64,24 @@ const Reducer = (state, action) => {
           },
         },
       };
+    case "DELETE_DEFECTURA_RECORD_MODAL":
+      // Data items:
+      // 0 - id of defectura item,
+      // 1 - function that handling deletion,
+      // 2 - function that reset modal state and close it
+      return {
+        ...state,
+        modal: {
+          showModal: true,
+          data: action.data[0],
+          content: "Delete defectura record?",
+          buttons: "YN",
+          handlers: {
+            btnYesHandler: action.data[1],
+            btnNoHandler: action.data[2],
+          },
+        },
+      };
     case "RESET_MODAL":
       return {
         ...state,
